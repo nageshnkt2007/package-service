57c730b22f6061b536f97c896ea0757b9ef7dcc7
Nagesh Tripathi <ntripathi@risesmart.com>
2019-12-13 16:56:52 +0530

    removed angular project from this repository.

--- shop-package-app/src/app/shared/product.model.ts
+++
@@ -1,9 +0,0 @@
-export class Product {
-  public id:number;
-  public name:string;
-  public productId:string;
-  public basePrice:number;
-
-  constructor(id:number,name:string,productId:string,basePrice:number) {
-  }
-}
