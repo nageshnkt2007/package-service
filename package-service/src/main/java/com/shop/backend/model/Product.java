3013d96ac5cf01348919eaa4aa87b456ac0d8289
Nagesh Tripathi <ntripathi@risesmart.com>
2019-12-16 20:23:19 +0530

    Package entity correction.

--- package-service/src/main/java/com/shop/backend/model/Product.java
+++ package-service/src/main/java/com/shop/backend/model/Product.java
@@ -15,7 +15,7 @@ public class Product implements Serializable {
      */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    private Long id;
 
     /**
      * name of package .
@@ -41,11 +41,11 @@ public class Product implements Serializable {
     private ShopPackage shopPackage;
 
 
-    public String getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
